b749e64b68bfdf3a43f6cbdd9f0366568af219ef
Phani Raj <phanirajuyn@gmail.com>
2016-01-27 11:47:16 -0500

    pick up branch from secrets file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-yes y | fly set-pipeline -p dogfood-ci-pipeline -c dogfood-ci-pipeline.yml --load-vars-from dogfood-ci-pipeline.python.secrets.yml
+yes y | fly set-pipeline -p dogfood-ci-pipeline -c dogfood-ci-pipeline.yml --load-vars-from dogfood-ci-pipeline.java.secrets.yml
 fly unpause-pipeline -p dogfood-ci-pipeline
 curl http://192.168.100.4:8080/pipelines/dogfood-ci-pipeline/jobs/job-clone-repo/builds -X POST
